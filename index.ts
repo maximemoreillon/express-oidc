f29cc55456545dcd6edf016872fb4372d9cf9d8c
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-22 16:32:27 +0900

    req.user

--- index.ts
+++ index.ts
@@ -1,49 +1,55 @@
-import { NextFunction, Request, Response } from "express"
-import createJwksClient from "jwks-rsa"
-import jwt from "jsonwebtoken"
+import { NextFunction, Request, Response } from "express";
+import createJwksClient from "jwks-rsa";
+import jwt from "jsonwebtoken";
 
 type Options = {
-  jwksUri: string
-  lax?: boolean
+  jwksUri: string;
+  lax?: boolean;
+};
+
+interface ReqWithUser extends Request {
+  user?: any;
+  jwt?: string;
 }
 
 const extractJwt = ({ headers, query }: Request) =>
   headers.authorization?.split(" ")[1] ??
   headers.authorization ??
   (query.jwt as string) ??
-  (query.token as string)
+  (query.token as string);
 
 export default ({ jwksUri, lax }: Options) => {
-  if (!jwksUri) throw new Error(`jwksUri not defined`)
+  if (!jwksUri) throw new Error(`jwksUri not defined`);
 
   const jwksClient = createJwksClient({
     jwksUri,
     cache: true,
     rateLimit: true,
-  })
+  });
 
-  return async (req: Request, res: Response, next: NextFunction) => {
+  return async (req: ReqWithUser, res: Response, next: NextFunction) => {
     try {
-      const token = extractJwt(req)
-      if (!token) throw new Error("Missing token")
+      const token = extractJwt(req);
+      if (!token) throw new Error("Missing token");
 
-      const decoded = jwt.decode(token, { complete: true })
-      if (!decoded) throw new Error(`Decoded token is null`)
+      const decoded = jwt.decode(token, { complete: true });
+      if (!decoded) throw new Error(`Decoded token is null`);
 
-      const kid = decoded?.header?.kid
-      if (!kid) throw new Error("Missing token kid")
+      const kid = decoded?.header?.kid;
+      if (!kid) throw new Error("Missing token kid");
 
-      const key = await jwksClient.getSigningKey(kid)
+      const key = await jwksClient.getSigningKey(kid);
 
-      const verified = jwt.verify(token, key.getPublicKey())
+      const user = jwt.verify(token, key.getPublicKey());
 
-      res.locals.user = verified
+      res.locals.user = user;
+      req.user = user;
 
-      next()
+      next();
     } catch (error: any) {
-      if (lax) return next()
-      console.error(error)
-      return res.status(401).send(error.toString())
+      if (lax) return next();
+      console.error(error);
+      return res.status(401).send(error.toString());
     }
-  }
-}
+  };
+};
